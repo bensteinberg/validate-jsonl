d71fdea2029da5e61b51f6084030a6c7d42afb36
bensteinberg <bsteinberg@law.harvard.edu>
2023-10-10 12:37:40 -0400

    Add fail-fast option

--- validate_jsonl/validate.py
+++ validate_jsonl/validate.py
@@ -7,7 +7,8 @@ from jsonschema import validate
 @click.command()
 @click.option('--schema', default='citations.json')
 @click.option('--jsonl', default='lines.jsonl')
-def cli(schema, jsonl):
+@click.option('--fail-fast/--no-fail-fast', default=False)
+def cli(schema, jsonl, fail_fast):
     """ Validates a JSONL file according to a schema """
 
     with open(schema, 'r') as f:
@@ -22,8 +23,12 @@ def cli(schema, jsonl):
                 validate(instance=json.loads(line), schema=schema)
             except Exception as e:
                 err = str(e).splitlines()[0]
-                exceptions[err].append(str(line_number))
-                click.echo(f'Exception at line {line_number}: {err}')
+                if fail_fast:
+                    msg = f'Failing fast at line {line_number}: {err}'
+                    raise click.ClickException(msg)
+                else:
+                    exceptions[err].append(str(line_number))
+                    click.echo(f'Exception at line {line_number}: {err}')
 
         s = 's' if line_number > 1 else ''
         click.echo(f'Info: Read {line_number} line{s} in {jsonl}.')
